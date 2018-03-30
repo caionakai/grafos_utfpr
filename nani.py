07e2052fe70b5b8ab806f3cbb5893ae4da77e001
Caio <kaioesperto@hotmail.com>
2018-03-29 22:05:59 -0300

    Update nani.py

--- nani.py
+++ nani.py
@@ -1,2 +1,19 @@
-#getOrder
-number_of_nodes(G);
+#GET ORDER
+import networkx as nx
+
+
+G = nx.Graph()
+
+G.add_node(1)
+G.add_node(2)
+
+G.add_edge(1,2)
+
+G.add_node(3)
+G.add_node(4)
+
+G.add_edge(1,3)
+G.add_edge(2,4)
+
+print G.number_of_nodes()
+print G.number_of_edges()
