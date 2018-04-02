4bf5130fbc18430fc17b2df2280adf3be3fb300f
João victor Nascimento <joao_victorxy@hotmail.com>
2018-04-02 20:34:39 -0300

    Seleção de nome de autores, nós de autores

--- nani.py
+++ nani.py
@@ -1,19 +1,44 @@
 #GET ORDER
 import networkx as nx
+import sys
 
 
 G = nx.Graph()
 
-G.add_node(1)
-G.add_node(2)
+cor = "branco"
+tempo = 0
+'''
+G.add_node("v1",cor='azul')
+G.add_node("v2",cor='perto')
 
-G.add_edge(1,2)
 
-G.add_node(3)
-G.add_node(4)
 
-G.add_edge(1,3)
-G.add_edge(2,4)
+G.nodes["v1"]['cor'] = 'amarelo'
+print (G.nodes.data())
 
-print G.number_of_nodes()
-print G.number_of_edges()
+'''
+
+def removeBreakLine(name):
+    if name[len(name)-1] == '.':
+       name = name[:-1]
+
+            
+argc = len(sys.argv)
+
+if argc != 3:
+    print 'Uso: python nani.py numero_de_artigos arquivo_dos_nome_dos_autores '
+    sys.exit(0)
+
+numero_de_artigos = sys.argv[1]
+aux = sys.argv[2]
+
+if 1 > numero_de_artigos > 100:
+    print 'Entrada Incorreta'
+
+arquivo = open(aux).readlines()
+
+for linha in arquivo:
+    token = linha.split(',')
+    for i in token:
+        if i != "0\n":
+            G.add_node(i, cor='Branca', tempo = 0)
