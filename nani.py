94e721f95b479e21e28b370c85b3b9c775ba1a9f
gabriel_choptian@hotmail.com <gabriel_choptian@hotmail.com>
2018-04-02 21:08:08 -0300

    funçao combinação de autores para criaçao de 'edges'

--- nani.py
+++ nani.py
@@ -18,27 +18,41 @@ print (G.nodes.data())
 
 '''
 
+def Combinacao_Dos_Autores(nomes):
+    novo_nome=[]
+    for linha in nomes:
+        for i in range(0 , len(linha)):
+            for j in range(i+1 , len(linha)):
+                novo_nome.append({linha[i].replace('\n',''),linha[j].replace('\n','')})
+    return novo_nome
+
 def removeBreakLine(name):
     if name[len(name)-1] == '.':
        name = name[:-1]
-
             
 argc = len(sys.argv)
 
 if argc != 3:
-    print 'Uso: python nani.py numero_de_artigos arquivo_dos_nome_dos_autores '
+    print ('Uso: python nani.py numero_de_artigos arquivo_dos_nome_dos_autores ')
     sys.exit(0)
 
 numero_de_artigos = sys.argv[1]
 aux = sys.argv[2]
 
-if 1 > numero_de_artigos > 100:
-    print 'Entrada Incorreta'
+#if 1 > numero_de_artigos > 100:
+#   print ('Entrada Incorreta')
 
 arquivo = open(aux).readlines()
 
+file=[]
+
 for linha in arquivo:
     token = linha.split(',')
+    file.append(token)
     for i in token:
         if i != "0\n":
             G.add_node(i, cor='Branca', tempo = 0)
+
+Combinacao_Dos_Autores(file)
+
+
